b43800b648f3bb8452a9966aff5add70a3763843
David Gaziev <davidgaziev6@gmail.com>
2023-08-03 16:06:35 +0600

    change dir

--- src/components/todocheck/TodoCheck.jsx
+++ src/components/todocheck/TodoCheck.jsx
@@ -1,10 +1,11 @@
 import './TodoCheck.css';
+import checkSVG from '../../static/icons/icon-check.svg';
 
 const TodoCheck = ({ isCompleted, callback }) => {
   return (
     <div className="todo-check">
       <button onClick={callback}>
-        {isCompleted ? <img src="src/assets/icons/icon-check.svg" /> : ''}
+        {isCompleted ? <img src={checkSVG} /> : ''}
       </button>
     </div>
   );
